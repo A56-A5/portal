c842433be4fced43dad92e0d3f3efed6b542f4f0
Alvi <alvivinod16@gmail.com>
2025-05-15 22:20:08 +0530

    mousekey fix1

--- mousekey-prototype/main/client_main.py
+++ mousekey-prototype/main/client_main.py
@@ -3,9 +3,11 @@ from net.client import Client
 from clipboard.clipboard_manager import watch_clipboard
 from input.input_listener import InputListener
 from common.protocol import encode_event
+import sys
 
 if __name__ == "__main__":
-    client = Client(server_ip='127.0.0.1')  # Change this dynamically via GUI or env
+    server_ip = sys.argv[1] if len(sys.argv) > 1 else "127.0.0.1"
+    client = Client(server_ip=server_ip)
     client.connect()
 
     listener = InputListener(send_callback=client.send)
