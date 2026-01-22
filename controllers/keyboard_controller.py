dbf613aab15c705679d682164319dd68b4accefa
Alvi <alvivinod16@gmail.com>
2026-01-22 20:06:42 +0530

    aight

--- controllers/keyboard_controller.py
+++ controllers/keyboard_controller.py
@@ -1,30 +1,23 @@
 import platform
 import time
-import ctypes
-from ctypes import wintypes
-from pynput.keyboard import Controller as PynputKeyboardController, Key
+from pynput.keyboard import Controller as PynputController, Key
 
 class KeyboardController:
     def __init__(self):
-        self._controller = PynputKeyboardController()
+        self._controller = PynputController()
         self.os_type = platform.system().lower()
-
-        self.win32api = None
-        self.win32con = None
         self.use_win32 = False
-        self.subprocess = None
         self.use_xdotool = False
+        self.subprocess = None
 
         if self.os_type == "windows":
             try:
-                import win32api
-                import win32con
+                import win32api, win32con
                 self.win32api = win32api
                 self.win32con = win32con
                 self.use_win32 = True
             except ImportError:
-                self.use_win32 = False
-
+                pass
         elif self.os_type == "linux":
             try:
                 import subprocess
@@ -32,121 +25,24 @@ class KeyboardController:
                 result = subprocess.run(["which", "xdotool"], capture_output=True)
                 self.use_xdotool = result.returncode == 0
             except:
-                self.use_xdotool = False
-
-    def press(self, key):
-        key_str = self._normalize_key(key)
-        if self.use_win32 and len(key_str) == 1:
-            self._win32_press(key_str)
-        elif self.use_xdotool:
-            self._xdotool_keydown(key_str)
-        else:
-            self._controller.press(key)
-
-    def release(self, key):
-        key_str = self._normalize_key(key)
-        if self.use_win32 and len(key_str) == 1:
-            self._win32_release(key_str)
-        elif self.use_xdotool:
-            self._xdotool_keyup(key_str)
-        else:
-            self._controller.release(key)
-
-    def tap(self, key):
-        key_str = self._normalize_key(key)
-        if self.use_win32 and len(key_str) == 1:
-            self._win32_tap(key_str)
-        elif self.use_xdotool:
-            self._xdotool_tap(key_str)
-        else:
-            self._controller.press(key)
-            self._controller.release(key)
+                pass
 
     def _normalize_key(self, key):
-        from pynput.keyboard import Key as PKey
-        if isinstance(key, PKey):
-            return f"Key.{key.name}"
+        if hasattr(key, "name"):
+            return key.name  # For Key.shift, Key.ctrl, etc.
         return str(key)
 
-    # -------- Linux xdotool methods --------
-    def _xdotool_tap(self, key_str):
-        xkey = self._key_to_xdotool(key_str)
-        if len(xkey) == 1:
-            self.subprocess.run(
-                ["xdotool", "type", "--delay", "0", "--clearmodifiers", xkey],
-                check=False
-            )
-        else:
-            self.subprocess.run(["xdotool", "key", xkey], check=False)
-
+    # ---------------- Linux ----------------
     def _xdotool_keydown(self, key_str):
-        xkey = self._key_to_xdotool(key_str)
-        self.subprocess.run(["xdotool", "keydown", xkey], check=False)
+        self.subprocess.run(["xdotool", "keydown", key_str], check=False)
 
     def _xdotool_keyup(self, key_str):
-        xkey = self._key_to_xdotool(key_str)
-        self.subprocess.run(["xdotool", "keyup", xkey], check=False)
+        self.subprocess.run(["xdotool", "keyup", key_str], check=False)
 
-    def _key_to_xdotool(self, key_str):
-        key_map = {
-            "Key.enter": "Return",
-            "Key.tab": "Tab",
-            "Key.space": "space",
-            "Key.esc": "Escape",
-            "Key.delete": "Delete",
-            "Key.backspace": "BackSpace",
-            "Key.ctrl": "Control_L",
-            "Key.ctrl_l": "Control_L",
-            "Key.ctrl_r": "Control_R",
-            "Key.alt": "Alt_L",
-            "Key.alt_l": "Alt_L",
-            "Key.alt_r": "Alt_R",
-            "Key.shift": "Shift_L",
-            "Key.shift_l": "Shift_L",
-            "Key.shift_r": "Shift_R",
-            "Key.super": "Super_L",
-            "Key.super_l": "Super_L",
-            "Key.super_r": "Super_R",
-            "ctrl": "Control_L",
-            "alt": "Alt_L",
-            "shift": "Shift_L",
-        }
-        return key_map.get(key_str, key_str)
+    def _xdotool_tap(self, key_str):
+        self.subprocess.run(["xdotool", "key", key_str], check=False)
 
-    # -------- Windows win32api methods --------
-    def _win32_tap(self, key_str):
-        vk_and_shift = self.win32api.VkKeyScan(ord(key_str))
-        if vk_and_shift == -1:
-            ucode = ord(key_str)
-            class KEYBDINPUT(ctypes.Structure):
-                _fields_ = [
-                    ("wVk", wintypes.WORD),
-                    ("wScan", wintypes.WORD),
-                    ("dwFlags", wintypes.DWORD),
-                    ("time", wintypes.DWORD),
-                    ("dwExtraInfo", wintypes.ULONG_PTR),
-                ]
-            class INPUT(ctypes.Structure):
-                _fields_ = [("type", wintypes.DWORD), ("ki", KEYBDINPUT)]
-            INPUT_KEYBOARD = 1
-            KEYEVENTF_UNICODE = 0x0004
-            KEYEVENTF_KEYUP = 0x0002
-            press = INPUT(INPUT_KEYBOARD, KEYBDINPUT(0, ucode, KEYEVENTF_UNICODE, 0, 0))
-            release = INPUT(INPUT_KEYBOARD, KEYBDINPUT(0, ucode, KEYEVENTF_UNICODE | KEYEVENTF_KEYUP, 0, 0))
-            ctypes.windll.user32.SendInput(1, ctypes.byref(press), ctypes.sizeof(press))
-            time.sleep(0.01)
-            ctypes.windll.user32.SendInput(1, ctypes.byref(release), ctypes.sizeof(release))
-            return
-        vk = vk_and_shift & 0xFF
-        shift_state = (vk_and_shift >> 8) & 0xFF
-        if shift_state & 0x01:
-            self.win32api.keybd_event(0x10, 0, 0, 0)
-        self.win32api.keybd_event(vk, 0, 0, 0)
-        time.sleep(0.01)
-        self.win32api.keybd_event(vk, 0, self.win32con.KEYEVENTF_KEYUP, 0)
-        if shift_state & 0x01:
-            self.win32api.keybd_event(0x10, 0, self.win32con.KEYEVENTF_KEYUP, 0)
-
+    # ---------------- Windows ----------------
     def _win32_press(self, key_str):
         vk = self._key_to_vk(key_str)
         if vk is not None:
@@ -157,8 +53,13 @@ class KeyboardController:
         if vk is not None:
             self.win32api.keybd_event(vk, 0, self.win32con.KEYEVENTF_KEYUP, 0)
 
+    def _win32_tap(self, key_str):
+        self._win32_press(key_str)
+        time.sleep(0.01)
+        self._win32_release(key_str)
+
     def _key_to_vk(self, key_str):
-        key_map = {
+        mapping = {
             "enter": 0x0D,
             "tab": 0x09,
             "space": 0x20,
@@ -173,27 +74,34 @@ class KeyboardController:
             "left": 0x25,
             "right": 0x27,
         }
-        return key_map.get(key_str.lower())
+        return mapping.get(key_str.lower())
 
-    # -------- Key parsing --------
-    def parse_key(self, key_str):
-        from pynput.keyboard import Key
-        KEY_ALIAS = {
-            "Key.ctrl": Key.ctrl_l,
-            "Key.ctrl_l": Key.ctrl_l,
-            "Key.ctrl_r": Key.ctrl_r,
-            "Key.shift": Key.shift_l,
-            "Key.shift_l": Key.shift_l,
-            "Key.shift_r": Key.shift_r,
-            "Key.alt": Key.alt_l,
-            "Key.alt_l": Key.alt_l,
-            "Key.alt_r": Key.alt_r,
-            "Key.cmd": Key.cmd_l,
-            "Key.cmd_l": Key.cmd_l,
-            "Key.cmd_r": Key.cmd_r,
-        }
-        if key_str in KEY_ALIAS:
-            return KEY_ALIAS[key_str]
-        if key_str.startswith("Key."):
-            return getattr(Key, key_str.split(".", 1)[1], None)
-        return key_str
+    # ---------------- Public API ----------------
+    def press(self, key):
+        key_str = self._normalize_key(key)
+        if self.use_win32:
+            self._win32_press(key_str)
+        elif self.use_xdotool:
+            self._xdotool_keydown(key_str)
+        else:
+            self._controller.press(key)
+
+    def release(self, key):
+        key_str = self._normalize_key(key)
+        if self.use_win32:
+            self._win32_release(key_str)
+        elif self.use_xdotool:
+            self._xdotool_keyup(key_str)
+        else:
+            self._controller.release(key)
+
+    def tap(self, key):
+        key_str = self._normalize_key(key)
+        if self.use_win32:
+            self._win32_tap(key_str)
+        elif self.use_xdotool:
+            self._xdotool_tap(key_str)
+        else:
+            self._controller.press(key)
+            time.sleep(0.01)
+            self._controller.release(key)
